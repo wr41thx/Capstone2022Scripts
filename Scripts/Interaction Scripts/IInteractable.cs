dbbaa614f8b131cfc3e78a64c07853ecb0dab470
Theo <34557859+wr41thx@users.noreply.github.com>
2022-11-27 12:31:34 -0800

    Delete Scripts directory
    going to replace

--- Scripts/Interaction Scripts/IInteractable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Intefrace for Interactable objects.  Scripts using IInteractable must use all methods and set/getters here.
-public interface IInteractable
-{
-    void Interact();
-}
